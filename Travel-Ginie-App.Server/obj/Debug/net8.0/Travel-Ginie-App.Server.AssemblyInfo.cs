d6344de8449bc98f9824340cd4673707ffc3809e
Skyeeer <charlie03.pl@gmail.com>
2024-05-15 13:26:23 +0200

    C# automatiska filer

--- Travel-Ginie-App.Server/obj/Debug/net8.0/Travel-Ginie-App.Server.AssemblyInfo.cs
+++ Travel-Ginie-App.Server/obj/Debug/net8.0/Travel-Ginie-App.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travel-Ginie-App.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27028b5522cc36fe6af835ac916c5ad4f38443e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d34a70632ee3c806e1c2c7f5e0a9e8c6c728c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travel-Ginie-App.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travel-Ginie-App.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
