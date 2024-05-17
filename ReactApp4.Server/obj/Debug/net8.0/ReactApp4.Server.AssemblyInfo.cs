2b9a90aecdfed80fef86a526556903cddda46579
Skyeeer <charlie03.pl@gmail.com>
2024-05-17 15:39:07 +0200

    Ändringar i launch

--- ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
+++ ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f163ff9e1c3a4a78b5bd748d462ed06c76440480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b4d8f469bfbcaa03303fefb59748f0afe65370")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
