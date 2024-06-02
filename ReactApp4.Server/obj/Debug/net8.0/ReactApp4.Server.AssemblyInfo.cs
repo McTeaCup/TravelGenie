66a91cd2aebb987ff90f082c45cbf3bd095e3039
Skyeeer <charlie03.pl@gmail.com>
2024-06-02 15:31:27 +0200

    Landing page ser fabolus ut

--- ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
+++ ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178e64c7491159c55d37647e932d7a3a3fdc1a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c15f637bbc1401153366f9e6faee9259a8753a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
