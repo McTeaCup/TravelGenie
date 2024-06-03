e859b40df872277e965b20c0afd2d9d28b273eb5
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-06-03 15:49:18 +0200

    merge dev landing into this branch

--- ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
+++ ReactApp4.Server/obj/Debug/net8.0/ReactApp4.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f219384b741ab2987442101d7667f56b7cadb1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf23d0f7ec0bc802f76d45590190ea38adb68bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp4.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
